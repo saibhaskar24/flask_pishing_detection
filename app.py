3290ec03863f58b901c7813b5cd56069d4a729e8
P Sai Bhaskar <pbbhsk@amazon.com>
2021-06-08 15:17:30 +0530

    ui edit

--- app.py
+++ app.py
@@ -9,12 +9,19 @@ app = Flask(__name__)
 def check():
     message = None
     website = None
+    is_good = None
+    is_bad = None
     if request.method == "POST":
-       url_data = request.form.get("url_data")
-       y = loader.check_is_pissing(url_data)
-       message = y[0]
-       website = url_data
-    return render_template("authentication.html",website = website, message = message)
+        url_data = request.form.get("url_data")
+        y = loader.check_is_pissing(url_data)
+        message = y[0]
+        website = url_data
+        if message == "good":
+            is_good = "True"
+        else:
+            is_bad = "True"
+
+    return render_template("authentication.html",website = website, message = message, is_good= is_good, is_bad=is_bad)
 
 if __name__=='__main__':
     app.run()
